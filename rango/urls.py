cff07bac5d6494a2e518e4256b392f56746fe1f0
carrot <huyw0721@outlook>
2021-07-27 09:56:13 +0100

    3.6 Add a about page to the rungo

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from rango import views
+from django.urls import path
 
 app_name = 'rango'
 urlpatterns = [
-url('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
 ]
